f021c1aadc48af534f48aa2a054e310b4a3caa65
snehal1158 <snehalkalkumbe11@gmail.com>
2025-07-09 09:52:24 +0530

    table.java
    table using printing statement

---
+++ table.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class table{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter the number to get its table:");
+		int a=sc.nextInt();
+		int b=2*a;
+		int c=a*3;
+		int d=a*4;
+		int e=a*5;
+		int f=a*6;
+		int g=a*7;
+		int h=a*8;
+		int i=a*9;
+		int j=a*10;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(i);
+		System.out.println(j);
+	
+	}
+}
