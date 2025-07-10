5b7fa13d0b9ede6f7677467902f66d451eaccda0
snehal1158 <snehalkalkumbe11@gmail.com>
2025-07-10 13:18:25 +0530

    ind.java
    commit

---
+++ java - Copy (2)/ind.java
@@ -0,0 +1,8 @@
+class ind{
+	public static void main (String[]args){
+		int num=2;
+		System.out.println(++num + num++);
+		//System.out.println("post inc:"+ num++);
+		//System.out.println("updated value:"+ num);
+	}
+}
