3a5d829e4f596d117f6fe9298de03cf86d8c89e3
snehal1158 <snehalkalkumbe11@gmail.com>
2025-07-10 13:05:17 +0530

    home.java
    commit

---
+++ java - Copy (2)/home.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class print{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter the radius:");
+		double radius=sc.nextInt();
+		double area=3.14*radius;
+		System.out.print(area);
+	}
+}
