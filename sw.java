cedc5e013712f5ede561faf4828db977d69033ff
snehal1158 <snehalkalkumbe11@gmail.com>
2025-07-14 12:30:00 +0530

    sw.java
    commit

---
+++ sw.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class sw{
+	public static void main(String[]args){
+		System.out.println("enter  number:");
+		Scanner sc=new Scanner(System.in);
+		//int a=sc.nextInt();
+		//int b=sc.nextInt();
+		int operator=sc.nextInt();
+		switch(operator){
+			case1:
+			{
+			System.out.println(january);
+			break;
+		     }
+			case2:
+			{
+			System.out.println(february);
+			break;
+		     }
+			case3:
+			{
+			System.out.println(march);
+			break;
+		     }
+			case4:
+			{
+			System.out.println(april);
+            break;
+             }
+	
+			default:
+            {
+            System.out.println("no information stored");
+            }
+
+		}
+
+	}
+
+}
